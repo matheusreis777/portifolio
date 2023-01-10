519e3752e04d609341cc3acbd5e711c0081d126f
matheusreis777 <93006729+matheusreis777@users.noreply.github.com>
2023-01-10 15:40:45 -0300

    modified:   index.html modified:   script.js

--- script.js
+++ script.js
@@ -35,8 +35,28 @@ $(document).ready(function () {
       })
     })
   });
-  
 
- function alerta(){
+
+function alerta(){
     alert("Ok!");
   }
+
+(function() {
+    // https://dashboard.emailjs.com/admin/account
+    emailjs.init('AZK1VA0C2NZjzrNge');
+})();
+
+window.onload = function() {
+  document.getElementById('contact-form').addEventListener('submit', function(event) {
+      event.preventDefault();
+      // generate a five digit number for the contact_number variable
+      this.contact_number.value = Math.random() * 100000 | 0;
+      // these IDs from the previous steps
+      emailjs.sendForm('service_h0i6cmb', 'template_cmwz5wj', this)
+          .then(function() {
+              console.log('SUCCESS!');
+          }, function(error) {
+              console.log('FAILED...', error);
+          });
+  });
+}
